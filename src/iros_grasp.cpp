292ced2d465b3af27aee62d77df8513fd82e34bc
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-02-21 23:50:02 +0100

    iros_grasp: randomize initial object pose + type

--- src/iros_grasp.cpp
+++ src/iros_grasp.cpp
@@ -1,13 +1,21 @@
 #include <stages/grasping_tasks.h>
 #include <ros/ros.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <random_numbers/random_numbers.h>
 #include "test_utils.h"
 
 using namespace moveit::task_constructor;
 using namespace moveit::task_constructor::stages;
+static random_numbers::RandomNumberGenerator rng;
 
-void spawnObjects(int primitive, geometry_msgs::PoseStamped& pose) {
+void spawnObjects(int primitive, geometry_msgs::PoseStamped& pose_msg) {
 	moveit::planning_interface::PlanningSceneInterface psi;
+	double quat[4];  // x y z w
+
+	// random rotation about z-axis
+	Eigen::Isometry3d pose = Eigen::AngleAxisd(rng.uniformReal(-M_PI/2., M_PI/2.), Eigen::Vector3d::UnitZ())
+	                         * Eigen::Translation3d::Identity();
 
 	double height = 0.25;
 	double radius = 0.03;
@@ -17,7 +25,7 @@ void spawnObjects(int primitive, geometry_msgs::PoseStamped& pose) {
 	o.primitive_poses.resize(1);
 	o.primitive_poses[0].position.x = -0.3;
 	o.primitive_poses[0].position.y = 0.30;
-	o.primitive_poses[0].position.z = height / 2.0 + 0.001;
+	o.primitive_poses[0].position.z = height / 2.0;
 	o.primitive_poses[0].orientation.w = 1.0;
 
 	o.primitives.resize(1);
@@ -25,32 +33,63 @@ void spawnObjects(int primitive, geometry_msgs::PoseStamped& pose) {
 	switch (primitive) {
 	case shape_msgs::SolidPrimitive::CYLINDER:
 		o.primitives[0].dimensions = {height, radius};
+		if (int lying = rng.uniformInteger(0, 1)) {
+			o.primitive_poses[0].position.z = radius;
+			pose = pose * Eigen::AngleAxisd(M_PI/2., Eigen::Vector3d::UnitY());
+		}
 		break;
 	case shape_msgs::SolidPrimitive::BOX:
-		o.primitives[0].dimensions = {4*radius, 3*radius, height};
+		o.primitives[0].dimensions = {3*radius, 2*radius, height};
+		switch (rng.uniformInteger(0, 2)) {
+		case 1:  // rotate about y axis
+			o.primitive_poses[0].position.z = o.primitives[0].dimensions[0] / 2.;
+			pose = pose * Eigen::AngleAxisd(M_PI/2., Eigen::Vector3d::UnitY());
+			break;
+		case 2:  // rotate about x axis
+			o.primitive_poses[0].position.z = o.primitives[0].dimensions[1] / 2.;
+			pose = pose * Eigen::AngleAxisd(M_PI/2., Eigen::Vector3d::UnitX());
+			break;
+		default:
+			break;
+		}
+		// avoid collision with table (bug in FixCollision / FCL for boxes)
+		o.primitive_poses[0].position.z += 0.001;
 		break;
 	case shape_msgs::SolidPrimitive::SPHERE:
 		o.primitives[0].dimensions = {radius};
 		o.primitive_poses[0].position.z = radius;
+		rng.quaternion(quat);  // random orientation
+		pose = Eigen::Quaterniond(quat);
 		break;
 	}
+	tf::quaternionEigenToMsg(Eigen::Quaterniond(pose.rotation()), o.primitive_poses[0].orientation);
 	psi.applyCollisionObject(o);
 
-	pose.header.frame_id = o.header.frame_id;
-	pose.pose = o.primitive_poses[0];
+	pose_msg.header.frame_id = o.header.frame_id;
+	pose_msg.pose = o.primitive_poses[0];
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 	ros::init(argc, argv, "pa10");
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 
-	geometry_msgs::PoseStamped pose;
-	spawnObjects(shape_msgs::SolidPrimitive::BOX, pose);
-	pose.pose.position.x = 0.3;
+	while (true) {
+		// spawn a random object and retrieve it's pose
+		geometry_msgs::PoseStamped pose;
+		spawnObjects(rng.uniformInteger(1, 3), pose);
 
-	auto task = std::unique_ptr<Task>(bimodalPickPlace(pose));
-	task->setProperty("object", std::string("object"));
-	task->plan();
-	waitForKey();
+		// place always at same position and orientation
+		pose.pose.position.x = 0.3;
+		pose.pose.position.y = 0.3;
+		pose.pose.position.z = 0.25 / 2.0 + 0.01;
+		tf::quaternionEigenToMsg(Eigen::Quaterniond::Identity(), pose.pose.orientation);
+
+		auto task = std::unique_ptr<Task>(bimodalPickPlace(pose));
+		task->setProperty("object", std::string("object"));
+		task->plan();
+
+		char ch; std::cin >> ch;
+		if (ch == 'q') break;
+	}
 }
